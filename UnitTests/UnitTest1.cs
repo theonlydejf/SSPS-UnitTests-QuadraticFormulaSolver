03103669ba2610a447035ed0881df6748464f312
theonlydejf <krcmar.dav@gmail.com>
2022-06-13 14:48:50 +0200

    Added UnitTests project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using SSPS_UnitTests_QuadraticFormulaSolver;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
