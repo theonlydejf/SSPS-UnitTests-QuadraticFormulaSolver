b140c60515cc05506dea73506417e1a6979af928
krcmar.da.2019 <krcmar.da.2019@UC313PC07>
2022-06-13 14:45:46 +0200

    Přidejte soubory projektu.

---
+++ SSPS_UnitTests_QuadraticFormulaSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSPS_UnitTests_QuadraticFormulaSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
