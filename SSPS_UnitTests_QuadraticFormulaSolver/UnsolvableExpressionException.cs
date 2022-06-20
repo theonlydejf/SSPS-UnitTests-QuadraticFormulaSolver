3d1a07032f13b120c76cc1d43a6fc4c7637cae50
theonlydejf <krcmar.dav@gmail.com>
2022-06-20 14:45:54 +0200

    Added UnsolvableExpressionException

---
+++ SSPS_UnitTests_QuadraticFormulaSolver/UnsolvableExpressionException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSPS_UnitTests_QuadraticFormulaSolver
+{
+    public class UnsolvableExpressionException : ApplicationException
+    {
+    }
+}
