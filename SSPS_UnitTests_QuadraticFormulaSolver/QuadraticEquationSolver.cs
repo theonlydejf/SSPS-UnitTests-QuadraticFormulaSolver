54a4f1feec393df62fe490cb08407bec6c94750b
theonlydejf <krcmar.dav@gmail.com>
2022-06-20 14:56:35 +0200

    Implemented SolveForZero

--- SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationSolver.cs
+++ SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationSolver.cs
@@ -13,12 +13,25 @@ namespace SSPS_UnitTests_QuadraticFormulaSolver
             _expression = expression;
         }
 
-        public QuadraticEquationResult SolveFor(double value)
+        public QuadraticEquationResult SolveForZero()
         {
-            throw new NotImplementedException();
-        }
+            double a = _expression.A;
+            double b = _expression.B;
+            double c = _expression.C;
 
-        public QuadraticEquationResult SolveForZero() => SolveFor(0);
+            double d = GetDiscriminant();
+
+            if (d < 0)
+                return QuadraticEquationResult.NULL;
+
+            if (d == 0)
+                return new QuadraticEquationResult(-b / (2 * a), -b / (2 * a));
+
+            double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+            double x2 = (-b - Math.Sqrt(d)) / 2 * a;
+
+            return new QuadraticEquationResult(x1, x2);
+        }
 
         public double GetDiscriminant() => _expression.B * _expression.B - 4 * _expression.A * _expression.C;
     }
