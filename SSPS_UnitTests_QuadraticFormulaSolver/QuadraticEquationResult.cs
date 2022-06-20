9dad61d174c54eb30c94965c45ad7c28b53eded7
theonlydejf <krcmar.dav@gmail.com>
2022-06-20 14:39:52 +0200

    Designed unit tests

--- SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationResult.cs
+++ SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationResult.cs
@@ -6,7 +6,33 @@ namespace SSPS_UnitTests_QuadraticFormulaSolver
 {
     public struct QuadraticEquationResult
     {
+        public static readonly QuadraticEquationResult NULL = new QuadraticEquationResult() { X1 = double.NaN, X2 = double.NaN };
+
         public double X1 { get; set; }
         public double X2 { get; set; }
+
+        public QuadraticEquationResult(double x1, double x2) : this()
+        {
+            X1 = x1;
+            X2 = x2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is QuadraticEquationResult))
+                return false;
+
+            QuadraticEquationResult result = (QuadraticEquationResult)obj;
+
+            return (X1 == result.X1 &&
+                   X2 == result.X2) ||
+                   (X2 == result.X1 &&
+                   X1 == result.X2);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X1, X2);
+        }
     }
 }
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -5,14 +5,59 @@ namespace UnitTests
 {
     public class Tests
     {
+        private static object[] ExpressionCases =
+        {
+            new object[] { new QuadraticExpression(1, 0, 1), QuadraticEquationResult.NULL, -4 },
+            new object[] { new QuadraticExpression(1, 0, 0), new QuadraticEquationResult(0, 0), 0 },
+            new object[] { new QuadraticExpression(1, 0, -1), new QuadraticEquationResult(-1, 1), 4 }
+        };
+
         [SetUp]
         public void Setup()
         {
+            /*_expressionNoResult = new ExpressionResultPair(new QuadraticExpression(1, 0, 1), QuadraticEquationResult.NULL, -4);
+            _expressionOneResult = new ExpressionResultPair(new QuadraticExpression(1, 0, 0), new QuadraticEquationResult(0, 0), 0);
+            _expressionTwoResults = new ExpressionResultPair(new QuadraticExpression(1, 0, -1), new QuadraticEquationResult(-1, 1), 4);*/
         }
 
         [Test]
-        public void Test1()
+        [TestCase(0, 1, 1, false)]
+        [TestCase(1, 1, 1, true)]
+        public void TestCreateExpression(double a, double b, double c, bool validExpression)
         {
+            TestDelegate testDelegate = () => new QuadraticExpression(a, b, c);
+            if(validExpression)
+                Assert.DoesNotThrow(testDelegate);
+            else
+                Assert.Throws<MissingExpressionMembersException>(testDelegate);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ExpressionCases))]
+        public void TestDiscriminant(QuadraticExpression expression, QuadraticEquationResult result, double expectedDiscriminant)
+        {
+            QuadraticEquationSolver solver = new QuadraticEquationSolver(expression);
+
+            double discriminant = solver.GetDiscriminant();
+
+            Assert.AreEqual(expectedDiscriminant, discriminant, 1E-6);
+        }
+
+        [Test]
+        public void TestResulEquality()
+        {
+            Assert.AreEqual(new QuadraticEquationResult(1, 2), new QuadraticEquationResult(2, 1));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ExpressionCases))]
+        public void TestResults(QuadraticExpression expression, QuadraticEquationResult expectedResult, double expectedDiscriminant)
+        {
+            QuadraticEquationSolver solver = new QuadraticEquationSolver(expression);
+
+            QuadraticEquationResult result = solver.SolveForZero();
+
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
