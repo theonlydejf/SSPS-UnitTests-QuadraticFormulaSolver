014e5790f163a6877fc7af68c8f034183b60fca5
theonlydejf <krcmar.dav@gmail.com>
2022-06-20 14:56:05 +0200

    Fixed QuadraticEquationResult.NULL not being equal

--- SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationResult.cs
+++ SSPS_UnitTests_QuadraticFormulaSolver/QuadraticEquationResult.cs
@@ -6,15 +6,18 @@ namespace SSPS_UnitTests_QuadraticFormulaSolver
 {
     public struct QuadraticEquationResult
     {
-        public static readonly QuadraticEquationResult NULL = new QuadraticEquationResult() { X1 = double.NaN, X2 = double.NaN };
+        public static readonly QuadraticEquationResult NULL = new QuadraticEquationResult() { X1 = double.NaN, X2 = double.NaN, isNull = true };
 
         public double X1 { get; set; }
         public double X2 { get; set; }
+
+        private bool isNull;
 
         public QuadraticEquationResult(double x1, double x2) : this()
         {
             X1 = x1;
             X2 = x2;
+            isNull = false;
         }
 
         public override bool Equals(object obj)
@@ -23,6 +26,9 @@ namespace SSPS_UnitTests_QuadraticFormulaSolver
                 return false;
 
             QuadraticEquationResult result = (QuadraticEquationResult)obj;
+
+            if (result.isNull && isNull)
+                return true;
 
             return (X1 == result.X1 &&
                    X2 == result.X2) ||
@@ -33,6 +39,11 @@ namespace SSPS_UnitTests_QuadraticFormulaSolver
         public override int GetHashCode()
         {
             return HashCode.Combine(X1, X2);
+        }
+
+        public override string ToString()
+        {
+            return $"{{x1={X1},x2={X2}}}";
         }
     }
 }
